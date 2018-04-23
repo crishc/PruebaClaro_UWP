b13a8c44c24c6cd8166eef5f8ca2470452faf75c
Cristopher Hernandez <chernandez@macroaplicaciones.com>
2018-04-23 13:04:28 -0500

    Detalles

--- PruebaClaro_UWP/View/Details.xaml.cs
+++ PruebaClaro_UWP/View/Details.xaml.cs
@@ -1,31 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
+﻿using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
-
-// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace PruebaClaro_UWP.View
 {
-    /// <summary>
-    /// Una página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
-    /// </summary>
     public sealed partial class Details : Page
     {
         public Details()
         {
             this.InitializeComponent();
         }
-        
+
+        //protected override void OnNavigatedTo(NavigationEventArgs e)
+        //{
+        //    base.OnNavigatedTo(e);
+
+        //    ConnectedAnimation imageAnimation =
+        //        ConnectedAnimationService.GetForCurrentView().GetAnimation("imagenPelicula");
+        //    if (imageAnimation != null)
+        //    {
+        //        imageAnimation.TryStart(Imagen);
+        //    }
+        //}
+
     }
 }
